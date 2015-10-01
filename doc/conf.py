7ddcf01dff4f923ecc8afaf5fefd2d829d4d812b
Fassio Blatter <fassio@specpose.com>
2015-10-01 16:59:32 +0100

    fixing documentation 17

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'stopeight'
-copyright = u'2012, Specific Purpose Software GmbH'
+copyright = u'2015, http://github.com/fassio/stopeight'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
