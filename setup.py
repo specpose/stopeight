57163bc0016b9e9502602b16a5cbfa4ba9dfb682
Fassio Blatter <fassio@specpose.com>
2016-01-29 11:34:30 +0000

    BEFORE refactoring for egg packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup( name='stopeight',
+       version='0.1.0',
+       description='stopeight: Comparing sequences of points in 2 dimensions by visually overlapping them using matrix transformations (translation, scaling and rotation) and getting a boolean result.',
+       author='Fassio Blatter',
+       license='GNU Lesser General Public License, version 2.1',
+       url='http://www.stopeight.com'
+)
