c17ed6857824d571d5b7b3d3fb033ab4a0301170
Fassio Blatter <fassio@specpose.com>
2016-08-15 16:09:23 +0100

    ADDED comparator Matrix tests

--- stopeight/comparator/matrixTools_new.py
+++ stopeight/comparator/matrixTools_new.py
@@ -1,6 +1,7 @@
-# Copyright (C) 2009-2012 Specific Purpose Software GmbH
+# Copyright (C) 2009-2016 Specific Purpose Software GmbH
 
 from numpy import array,pi,cos,sin
+from numpy import sqrt,exp
 #from SVG import Scene
 #from numpy.oldnumeric.ma import dot
 from numpy import dot
@@ -12,39 +13,34 @@ _deg_to_rad = lambda deg: 2 * pi / (360.0/deg)
 def idmatrix():
     return array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
-def translate(a):
-    m = array([[1,0,0],[0,1,0],[a[0],a[1],1]])
-    return m
+class Matrix:
+    def __init__(self,translation=[0,0],rotation=0):
+        self.x = translation[0]
+        self.y = translation[1]
+        self.rot = _deg_to_rad(rotation)
 
-def scale(a):
-    m = array([[a[0],0,0],[0,a[1],0],[0,0,1]])
-    return m
+#def scale(a):
+#    m = array([[a[0],0,0],[0,a[1],0],[0,0,1]])
+#    return m
 
-def rotate(deg):
-    if (deg!=0):
-        c = cos(_deg_to_rad(deg))
-        s = sin(_deg_to_rad(deg))
-        m = array([[c,s,0],[-s,c,0],[0,0,1]])
-    else:
-        m = idmatrix()
-    return m
-
+# row first
 def transform(a, m):
     b = array([0,0])
-    b[0]= a[0] * m[0][0] + a[1]* m[1][0] + m[2][0]
-    b[1]= a[0] * m[0][1] + a[1]* m[1][1] + m[2][1]
+    b[0] = cos(m.rot) * a[0] + -sin(m.rot) * a[1] + m.x
+    b[1] = sin(m.rot) * a[0] + cos(m.rot) * a[1] + m.y
     return b
 
-def center(a):
-    return transform(a,translate(array([0,0])))
+#def center(a):
+#    return transform(a,translate(array([0,0])))
 
 if __name__ == '__main__':
     a = array([30,30])
-    m = idmatrix()
+    #m = idmatrix()
     print ('rotate vector with matrix:')
-    m = dot(translate(array([10,15])),m)
+    #m = dot(translate(array([10,15])),m)
     #m = dot(scale(array([0.5,0.5])),m)
-    m = dot(rotate(-90),m)
+    #m = dot(rotate(-90),m)
+    m = Matrix([10,15],-90)
     result = transform(a,m)
     print(a)
     print(result)
