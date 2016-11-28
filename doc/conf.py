322839342200126d55920d55b85dc9e3b94c7a30
Fassio Blatter <fassio@specpose.com>
2016-11-28 18:36:23 +0000

    ADDED Grapher documentation

--- doc/conf.py
+++ doc/conf.py
@@ -23,9 +23,11 @@ sys.path.insert(0, os.path.abspath('../'))
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
+imgmath_image_format = 'svg'
+
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.viewcode','sphinx.ext.imgmath']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -94,13 +96,23 @@ autoclass_content = 'class'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
+#default theme
+#html_theme_options = {
+#"sidebarbgcolor": "white"
+#}
+#alabaster
 html_theme_options = {
-"sidebarbgcolor": "white"
+"logo_name": "true",
+"github_banner": "true",
+"github_user": "specpose",
+"github_repo": "stopeight",
+"fixed_sidebar": "true",
+"sidebar_includehidden": "true",
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -115,7 +127,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'crab.png'
+html_logo = '_static/crab.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
