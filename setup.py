4fea353fca344dd93e6cd4d043ca2f846afb3662
Fassio Blatter <fassio@specpose.com>
2017-10-15 00:05:14 +0100

    REFACTORING moved analyzer scripts to util, ADDED python legacy build

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ _packages = [ 'stopeight','stopeight.logging','stopeight.comparator','stopeight.
 
 import os
 #cmake
-__import__('cmake')
-from cmake import CMakeExtension, CMakeBuild
+#__import__('cmake')
+#from cmake import CMakeExtension, CMakeBuild
 #or distutils
 #__import__('monkey_patch_parallel')
-#__import__('python')
-#from python import get_pybind_include, BuildExt
-#my_path = os.path.dirname(os.path.realpath(__file__))
+__import__('python')
+from python import get_pybind_include, BuildExt
+my_path = os.path.dirname(os.path.realpath(__file__))
 
 from setuptools import setup, Extension
 
@@ -22,34 +22,44 @@ setup( name='stopeight',
        url='http://www.stopeight.com',
        packages=_packages,
 
-       ext_modules=[CMakeExtension('stopeight')],#,os.path.join('stopeight-clibs','grapher-wrappers'))],
-       cmdclass=dict(build_ext=CMakeBuild),
-#       ext_modules = [
-#           Extension(
-#               'stopeight.grapher',
-#               [os.path.join(my_path,'stopeight-clibs','grapher-wrappers','interfacepython.cpp')],
-#               include_dirs=[
-#                   # Path to pybind11 headers
-#                   get_pybind_include(),
-#                   get_pybind_include(user=True),
-#                   os.path.join(my_path,'stopeight-clibs','include'),
-#                   os.path.join(my_path,'stopeight-clibs','grapher')
-#               ],
-#               libraries=['stopeight-clibs-grapher'],
-#               language='c++'
-#           ),
-#           Extension(
-#               'stopeight.analyzer',
-#               [os.path.join(my_path,'stopeight-clibs','analyzer-wrappers','interfacepython.cpp')],
-#               include_dirs=[
-#                   os.path.join(my_path,'stopeight-clibs','analyzer/include')
-#               ],
-#               libraries=['stopeight-clibs-analyzer'],
-#               language='c++'
-#           ),
-#       ],
-#       install_requires=['pybind11>=2.2'],
-#       cmdclass={'build_ext': BuildExt},
+#       ext_modules=[CMakeExtension('stopeight')],#,os.path.join('stopeight-clibs','grapher-wrappers'))],
+#       cmdclass=dict(build_ext=CMakeBuild),
+       ext_modules = [
+           Extension(
+               'stopeight.grapher',
+               [os.path.join(my_path,'stopeight-clibs','grapher-wrappers','interfacepython.cpp')],
+               include_dirs=[
+                   # Path to pybind11 headers
+                   get_pybind_include(),
+                   get_pybind_include(user=True),
+                   os.path.join(my_path,'stopeight-clibs','include'),
+                   os.path.join(my_path,'stopeight-clibs','grapher')
+               ],
+               libraries=['stopeight-clibs-grapher'],
+               language='c++'
+           ),
+           Extension(
+               'stopeight.analyzer',
+               [os.path.join(my_path,'stopeight-clibs','analyzer-wrappers','interfacepython.cpp')],
+               include_dirs=[
+                   os.path.join(my_path,'stopeight-clibs','analyzer/include')
+               ],
+               libraries=['stopeight-clibs-analyzer'],
+               language='c++'
+           ),
+           Extension(
+               'stopeight.legacy',
+               [os.path.join(my_path,'stopeight-clibs','legacy-wrappers','interfacepython.cpp')],
+               include_dirs=[
+                   os.path.join(my_path,'stopeight-clibs','legacy/include')
+               ],
+#               libraries=['QtCore','QtGui','stopeight-clibs-legacy'],
+               libraries=['stopeight-clibs-legacy'],
+               language='c++'
+           ),
+       ],
+       install_requires=['pybind11>=2.2'],
+       cmdclass={'build_ext': BuildExt},
 
        zip_safe=False,
 )
