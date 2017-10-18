c4331e66d2396343fb1025b449fbcd59d800827e
Fassio Blatter <fassio@specpose.com>
2017-10-18 23:44:46 +0100

    ADDED plotwav in wave_area

--- stopeight/util/wave_area.py
+++ stopeight/util/wave_area.py
@@ -1,4 +1,6 @@
 # https://stackoverflow.com/questions/12459811/how-to-embed-matplotlib-in-pyqt-for-dummies
+# https://stackoverflow.com/questions/18625085/how-to-plot-a-wav-file
+
 import sys
 from PyQt5 import QtGui,QtWidgets
 
@@ -8,9 +10,9 @@ from matplotlib.figure import Figure
 
 import random
 
-class Window(QtWidgets.QDialog):
+class WaveArea(QtWidgets.QWidget):
     def __init__(self, parent=None):
-        super(Window, self).__init__(parent)
+        super(WaveArea, self).__init__(parent)
 
         # a figure instance to plot on
         self.figure = Figure()
@@ -34,27 +36,51 @@ class Window(QtWidgets.QDialog):
         layout.addWidget(self.button)
         self.setLayout(layout)
 
+        self.INPUT = []
+        self.OUTPUT = []
+
     def plot(self):
         ''' plot some random stuff '''
-        # random data
-        data = [random.random() for i in range(10)]
+        import matplotlib.pyplot as ax
+        import numpy as np
+        import wave
+        import sys
+
+        print("Opening "+sys.argv[1])
+        spf = wave.open(sys.argv[1],'r')
+
+        #Extract Raw Audio from Wav File
+        signal = spf.readframes(-1)
+        signal = np.fromstring(signal, 'Int16')
+
+        #If Stereo
+        if spf.getnchannels() == 2:
+            print("Just mono files")
+            sys.exit(0)
 
         # create an axis
         ax = self.figure.add_subplot(111)
+        #ax.figure(1)
 
         # discards the old graph
-        ax.clear()
+        #ax.clear()
 
-        # plot data
-        ax.plot(data, '*-')
+        #ax.title("Signal Wave...")
+        ax.plot(signal)
 
+        #ax.set_ylabel([])
+        #ax.set_xlabel([])
+
+        # Turn off tick labels
+        #ax.set_yticklabels([])
+        #ax.set_xticklabels([])
+
+        #ax.show()
         # refresh canvas
         self.canvas.draw()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-
-    main = Window()
+    main = WaveArea()
     main.show()
-
     sys.exit(app.exec_())
