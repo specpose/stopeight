e4cf84bead06bffe37639c64b479cff2c2f31913
Fassio Blatter <fassio@specpose.com>
2018-08-05 00:37:54 +0100

    ADDED grapher module stubs

--- stopeight/util/editor/modules/grapher.py
+++ stopeight/util/editor/modules/grapher.py
@@ -1,1 +1,13 @@
 # Copyright (C) 2018 Fassio Blatter
+
+import stopeight.grapher
+from stopeight.util.editor.data import ScribbleData, WaveData
+
+from stopeight.logging import logSwitch
+log = logSwitch.logPrint()
+
+def samples_To_VG(data):
+    log.info("Loading with "+str(len(data)))
+    algo = stopeight.grapher.VectorGraph(1,1.0,[])
+    return algo(data)
+samples_To_VG.__annotations__ = {'data':WaveData,'return':ScribbleData}
