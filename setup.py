8cc608fd27fe7649a0279ff4b738f38960f835eb
Fassio Blatter <fassio@specpose.com>
2020-02-27 02:47:03 +0100

    FIXED setup.py linux qt include dirs

--- setup.py
+++ setup.py
@@ -5,10 +5,24 @@ _packages = [ 'stopeight','stopeight.logging','stopeight.comparator','stopeight.
 import os
 #distutils start
 #__import__('monkey_patch_parallel')
-__import__('python')
-from python import get_pybind_include, BuildExt
-my_path = os.path.dirname(os.path.realpath(__file__))
-from distutils import sysconfig
+import sys
+if len(sys.argv)>1:
+    if sys.argv[1]=='build_ext':
+        __import__('python')
+        from python import get_pybind_include, BuildExt
+        my_path = os.path.dirname(os.path.realpath(__file__))
+        from distutils import sysconfig
+        _include_dirs=[
+            os.path.join(my_path,'stopeight-clibs','cmake-git-version-tracking','better-example'),
+            # Path to pybind11 headers
+            get_pybind_include(),
+            get_pybind_include(user=True),
+        ]
+        _qt5_include_dirs=[]
+        if sysconfig.get_config_var('INCLUDEDIR')!='None':
+            _qt5_include_dirs.append(os.path.join(str(sysconfig.get_config_var('INCLUDEDIR')),str(sysconfig.get_config_var('MULTIARCH')),'qt5')) #ubuntu only
+            _qt5_include_dirs.append(os.path.join(str(sysconfig.get_config_var('INCLUDEDIR')),str(sysconfig.get_config_var('MULTIARCH'))))
+            _qt5_include_dirs.append(os.path.join(str(sysconfig.get_config_var('INCLUDEDIR'))))
 #distutils end
 
 from setuptools import setup, Extension
@@ -38,14 +52,10 @@ setup( use_scm_version=True,
            Extension(
                'stopeight.grapher',
                [os.path.join(my_path,'stopeight-clibs','grapher-wrappers','IFPyGrapher.cpp')],
-               include_dirs=[
-                   # Path to pybind11 headers
-                   get_pybind_include(),
-                   get_pybind_include(user=True),
+               include_dirs=_include_dirs.append([
                    os.path.join(my_path,'stopeight-clibs','include'),
-                   os.path.join(my_path,'stopeight-clibs','grapher'),
-		   os.path.join(my_path,'stopeight-clibs','cmake-git-version-tracking','better-example')
-               ],
+                   os.path.join(my_path,'stopeight-clibs','grapher')
+               ]),
                libraries=['stopeight-clibs-grapher'],
                language='c++',
                optional=True
@@ -53,14 +63,10 @@ setup( use_scm_version=True,
            Extension(
                'stopeight.matrix',
                [os.path.join(my_path,'stopeight-clibs','matrix-wrappers','IFPyMatrix.cpp')],
-               include_dirs=[
-                   # Path to pybind11 headers
-                   get_pybind_include(),
-                   get_pybind_include(user=True),
+               include_dirs=_include_dirs.append([
                    os.path.join(my_path,'stopeight-clibs','include'),
-                   os.path.join(my_path,'stopeight-clibs','matrix'),
-		   os.path.join(my_path,'stopeight-clibs','cmake-git-version-tracking','better-example')
-               ],
+                   os.path.join(my_path,'stopeight-clibs','matrix')
+               ]),
                libraries=['stopeight-clibs-matrix'],
                language='c++',
                optional=True
@@ -68,14 +74,10 @@ setup( use_scm_version=True,
            Extension(
                'stopeight.analyzer',
                [os.path.join(my_path,'stopeight-clibs','analyzer-wrappers','IFPyAnalyzer.cpp')],
-               include_dirs=[
-                   # Path to pybind11 headers
-                   get_pybind_include(),
-                   get_pybind_include(user=True),
+               include_dirs=_include_dirs.append([
                    os.path.join(my_path,'stopeight-clibs','include'),
-                   os.path.join(my_path,'stopeight-clibs','analyzer'),
-		   os.path.join(my_path,'stopeight-clibs','cmake-git-version-tracking','better-example')
-               ],
+                   os.path.join(my_path,'stopeight-clibs','analyzer')
+               ]),
                libraries=['stopeight-clibs-analyzer'],
                language='c++',
                optional=True
@@ -83,11 +85,9 @@ setup( use_scm_version=True,
            Extension(
               'stopeight.legacy',
                [os.path.join(my_path,'stopeight-clibs','legacy-wrappers','interfacepython.cpp')],
-               include_dirs=[
-                   os.path.join(my_path,'stopeight-clibs','legacy/include'),
-		   os.path.join(my_path,'stopeight-clibs','cmake-git-version-tracking','better-example'),
-                   os.path.join(str(sysconfig.get_config_var('INCLUDEDIR')),str(sysconfig.get_config_var('MULTIARCH')),'qt5')#ubuntu only
-               ],
+               include_dirs=_include_dirs.append([
+                   os.path.join(my_path,'stopeight-clibs','legacy/include')
+               ]).append(_qt5_include_dirs),
                libraries=['stopeight-clibs-legacy'],
                language='c++',
                optional=True
