87762000f2d307f35c6d97856a8cc466a39c162f
Fassio Blatter <fassio@specpose.com>
2020-05-20 09:18:52 +0200

    REMOVED legacy test_Parallel

--- stopeight/test/test_legacy.py
+++ stopeight/test/test_legacy.py
@@ -6,10 +6,10 @@ def test_Sequential():
     log.setLevel(log.CRITICAL)
     lines = process_directory(os.path.join('stopeight-clibs','legacy','test-data'),'.sp',legacy._parse_file,legacy.stroke_sequential,filename='Sequential.out',drawResize=False)
 
-def test_Parallel():
-    from stopeight.util.editor.modules import legacy
-    from stopeight.util.parser import process_directory
-    import os
-    import stopeight.logging as log
-    log.setLevel(log.CRITICAL)
-    lines = process_directory(os.path.join('stopeight-clibs','legacy','test-data'),'.sp',legacy._parse_file,legacy.stroke_parallel,filename='Parallel.out',drawResize=False)
+#def test_Parallel():
+#    from stopeight.util.editor.modules import legacy
+#    from stopeight.util.parser import process_directory
+#    import os
+#    import stopeight.logging as log
+#    log.setLevel(log.CRITICAL)
+#    lines = process_directory(os.path.join('stopeight-clibs','legacy','test-data'),'.sp',legacy._parse_file,legacy.stroke_parallel,filename='Parallel.out',drawResize=False)
