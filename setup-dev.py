5cc2943a5d3a70aadf148d7d51189d4dcf1065de
Fassio Blatter <fassio@specpose.com>
2023-04-24 19:33:39 +0100

    REFACTORING using pypa build with python_example

---
+++ setup-dev.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+#cmake start
+__import__('cmake')
+from cmake import CMakeExtension, CMakeBuild
+#cmake end
+
+from setuptools import setup, Extension, find_namespace_packages
+
+setup( use_scm_version=True,
+       packages=find_namespace_packages(include=['stopeight.*']),
+#cmake start
+#Hack for faster compile. --inplace and copy_extensions_to_source to working anyway
+#       ext_modules=[CMakeExtension('stopeight.matrix',''),CMakeExtension('stopeight.grapher',''),CMakeExtension('stopeight.analyzer',''),CMakeExtension('stopeight.legacy','')],
+       ext_modules=[CMakeExtension('stopeight.stopeight-clibs','')],
+       cmdclass=dict(build_ext=CMakeBuild),
+#cmake end
+)
--- setup.py
+++ setup.py
@@ -1,32 +1,94 @@
 #!/usr/bin/env python
 
 import os
-#cmake start
-__import__('cmake')
-from cmake import CMakeExtension, CMakeBuild
-#cmake end
+_include_dirs=[
+    os.path.join('stopeight-clibs','cmake-git-version-tracking','better-example'),
+]
+_qt5_include_dirs=_include_dirs
+_library_dirs=[]
 
-from setuptools import setup, Extension, find_namespace_packages
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, find_namespace_packages
 
 setup( use_scm_version=True,
        packages=find_namespace_packages(include=['stopeight.*']),
-       entry_points={
-           'setuptools.installation':['eggsecutable = stopeight.util.editor.dispatch:main_func',]
-           },
-       setup_requires=[
-           'setuptools_scm',
-           ],
-#pip start - not in cmake_examples
-       install_requires=[
-          'numpy',
-          'matplotlib',
-          'PySide2',
-          ],
-#pip end - not in cmake_examples
-#cmake start
-#Hack for faster compile. --inplace and copy_extensions_to_source to working anyway
-#       ext_modules=[CMakeExtension('stopeight.matrix',''),CMakeExtension('stopeight.grapher',''),CMakeExtension('stopeight.analyzer',''),CMakeExtension('stopeight.legacy','')],
-       ext_modules=[CMakeExtension('stopeight.stopeight-clibs','')],
-       cmdclass=dict(build_ext=CMakeBuild),
-#cmake end
+       ext_modules = [
+           Pybind11Extension(
+               'stopeight.grapher',
+               [os.path.join('stopeight-clibs','grapher','python','IFPyGrapher.cpp')],
+               include_dirs=_include_dirs + [
+                   os.path.join('stopeight-clibs','include'),
+                   os.path.join('stopeight-clibs','grapher')
+               ],
+               library_dirs=_library_dirs,
+               libraries=['stopeight-clibs-grapher',
+               'stopeight-clibs-matrix'],
+               language='c++',
+               optional=False
+           ),
+           Pybind11Extension(
+               'stopeight.matrix',
+               [os.path.join('stopeight-clibs','matrix','python','IFPyMatrix.cpp')],
+               include_dirs=_include_dirs + [
+                   os.path.join('stopeight-clibs','include'),
+                   os.path.join('stopeight-clibs','matrix')
+               ],
+               library_dirs=_library_dirs,
+               libraries=['stopeight-clibs-matrix'],
+               language='c++',
+               optional=False
+           ),
+           Pybind11Extension(
+               'stopeight.analyzer',
+               [os.path.join('stopeight-clibs','analyzer','python','IFPyAnalyzer.cpp')],
+               include_dirs=_include_dirs + [
+                   os.path.join('stopeight-clibs','include'),
+                   os.path.join('stopeight-clibs','analyzer')
+               ],
+               library_dirs=_library_dirs,
+               libraries=['stopeight-clibs-analyzer'],
+               language='c++',
+               optional=True
+           ),
+           Pybind11Extension(
+              'stopeight.legacy',
+               [os.path.join('stopeight-clibs','legacy','python','interfacepython.cpp'),
+               ],
+               include_dirs=_qt5_include_dirs + [
+                   os.path.join('stopeight-clibs','legacy/include'),
+                   os.path.join('stopeight-clibs','legacy','python')
+               ],
+               library_dirs=_library_dirs,
+               libraries=['stopeight-clibs-legacy','Qt5Core'],#Qt5Core needed for old wrapper
+               language='c++',
+               optional=True
+              ),
+            Pybind11Extension(
+              'stopeight.getters',
+               [os.path.join('stopeight-clibs','legacy','python','IFPyGetters.cpp'),
+               os.path.join('stopeight-clibs','legacy','python','IFPyShared.cpp')],
+               include_dirs=_qt5_include_dirs + [
+                   os.path.join('stopeight-clibs','legacy/include'),
+                   os.path.join('stopeight-clibs','legacy','python')
+               ],
+               library_dirs=_library_dirs,
+               libraries=['stopeight-clibs-legacy','stopeight-clibs-matrix','Qt5Core'],#Qt5Core needed for old wrapper
+               language='c++',
+               optional=True
+              ),
+            Pybind11Extension(
+              'stopeight.finders',
+               [os.path.join('stopeight-clibs','legacy','python','IFPyFinders.cpp'),
+               os.path.join('stopeight-clibs','legacy','python','IFPyShared.cpp')],
+               include_dirs=_qt5_include_dirs + [
+                   os.path.join('stopeight-clibs','legacy/include'),
+                   os.path.join('stopeight-clibs','legacy','python')
+               ],
+               library_dirs=_library_dirs,
+               libraries=['stopeight-clibs-legacy','stopeight-clibs-matrix','Qt5Core'],#Qt5Core needed for old wrapper
+               language='c++',
+               optional=True
+              ),
+       ],
+       cmdclass={'build_ext': build_ext},
 )
